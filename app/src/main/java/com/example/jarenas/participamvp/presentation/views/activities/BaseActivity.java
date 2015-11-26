5c12aef0ec5d9ed5688b9521dd42009c7e6b262d
Jolete <jordi_arenas@hotmail.com>
2015-11-26 01:09:54 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/jarenas/participamvp/presentation/views/activities/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.example.jarenas.participamvp.presentation.views.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by jarenas on 25/11/2015.
+ */
+
+public abstract class BaseActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
